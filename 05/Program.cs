1dfcf1a414918ca010dd16c7ea4ecb17ca5bda6b
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-04-10 12:07:41 -0600

    05

---
+++ 05/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//Desarrolle un programa que solicite un número entero y determine si es par o impar,
+//validando previamente que el dato ingresado sea correcto
+int numero;
+bool validarNumero = false;
+do
+{
+    Console.WriteLine("Ingrese un número: ");
+    validarNumero = int.TryParse(Console.ReadLine(), out numero);
+    if (validarNumero)
+    {
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"El número {numero} es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numero} es impar.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Valor ingresado no es un número entero válido. Intente nuevamente.");
+        validarNumero = false;
+    }
+}
+while (validarNumero != true);
