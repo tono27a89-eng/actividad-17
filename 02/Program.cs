2015420df85b29d694b262d61700c006c532ab53
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-04-10 08:46:58 -0600

    mmm

--- 02/Program.cs
+++ 02/Program.cs
@@ -1,3 +1,20 @@
 ﻿
 //Desarrolle un programa que solicite el precio de un producto, validando que sea un número decimal mayor que 0,
 //repitiendo hasta que el usuario ingrese correctamente el valor
+
+decimal precio;
+bool validarPrecio = false;
+do
+{
+    Console.WriteLine("ingrese precio: ");validarPrecio = decimal.TryParse(Console.ReadLine(), out precio);
+    if (validarPrecio && precio > 0)
+    {
+        Console.WriteLine("Precio ingresado: " + precio);
+    }
+    else
+    {
+        Console.WriteLine("Valor ingresado no es un número decimal válido. Intente nuevamente.");
+        validarPrecio = false;
+    }
+}
+while (validarPrecio!=true);
