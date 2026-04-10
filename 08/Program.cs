47fbde340682211feb20f538533e7cd28f625f5d
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-04-10 14:22:08 -0600

    09

--- 08/Program.cs
+++ 08/Program.cs
@@ -1,3 +1,21 @@
 ﻿
 //Desarrolle un programa que solicite un número y calcule su raíz cuadrada,
 //validando que el valor ingresado sea numérico y no negativo
+double numero;  
+bool NumeroValido = false;
+do
+{
+    Console.WriteLine("Ingrese un numero positivo: "); NumeroValido = double.TryParse(Console.ReadLine(), out numero);
+    if (NumeroValido && numero > 0)
+    {
+        double raizCuadrada = Math.Sqrt(numero);
+        Console.WriteLine($"La raíz cuadrada de {numero} es: {raizCuadrada:F2}");
+
+    }
+    else
+    {
+        Console.WriteLine("El valor ingresado no es un número válido o es negativo. Por favor, intente nuevamente.");
+        NumeroValido = false;
+    }
+}
+while (NumeroValido!=true);
---
+++ 09/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//Desarrolle un programa que solicite la nota de un estudiante,
+//validando que sea un número entre 0 y 100 antes de mostrar si aprueba o reprueba
+int nota;   
+bool NotaValida = false;
+do
+{
+    Console.WriteLine("Ingrese la nota del estudiante (0-100): ");
+    NotaValida = int.TryParse(Console.ReadLine(), out nota);
+    if (NotaValida && nota >= 0 && nota <= 100)
+    {
+        if (nota >= 60)
+        {
+            Console.WriteLine("El estudiante aprueba.");
+        }
+        else
+        {
+            Console.WriteLine("El estudiante reprueba.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("La nota ingresada no es válida. Por favor, ingrese un número entre 0 y 100.");
+        NotaValida = false;
+    }
+}while (!NotaValida);
