2daad139fee42d0a2e4678b08b942ccde1db7951
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-04-10 08:37:03 -0600

    1

---
+++ 01/Program.cs
@@ -0,0 +1,20 @@
+﻿//Desarrolle un programa que solicite la edad de una persona, validando que el valor ingresado sea un número entero válido,
+//repitiendo hasta que el dato sea correcto y mostrando la edad ingresada
+
+int edad;
+bool validaredad=false;
+do
+{
+    Console.WriteLine("ingrese edad: ");validaredad= int.TryParse(Console.ReadLine(), out edad);
+    if (validaredad)
+    {
+        Console.WriteLine("Edad ingresada: " + edad);
+    }
+    else
+    {
+        Console.WriteLine("Valor ingresado no es un número entero válido. Intente nuevamente.");
+    }
+
+    }
+while (validaredad!=true);
+
