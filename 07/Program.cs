f654dd2ed01691f1a49c9ef16c73652f0a2d580e
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-04-10 12:24:55 -0600

    07

---
+++ 07/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+//Desarrolle un programa que solicite el salario de un empleado,
+//validando que sea un número decimal válido mayor o igual a 0
+decimal salario;
+bool validarSalario = false;
+do
+{
+    Console.WriteLine("Ingrese el salario del empleado: ");validarSalario = decimal.TryParse(Console.ReadLine(), out salario);
+    if (validarSalario && salario >= 0)
+    {
+        Console.WriteLine($"Salario ingresado: {salario:F2}");
+    }
+    else
+    {
+        Console.WriteLine("Valor ingresado no es un número decimal válido. Intente nuevamente.");
+        validarSalario = false;
+    }
+}
+while (validarSalario!= true);
