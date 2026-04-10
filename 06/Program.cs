e98710e151f33195aae792dcd7fdc36e6179176e
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-04-10 12:11:10 -0600

    pruebas  tengo mis dudas

--- 06/Program.cs
+++ 06/Program.cs
@@ -1,3 +1,19 @@
 ﻿
 //Desarrolle un programa que solicite una fecha,
 //validando que el formato ingresado sea correcto, y luego mostrarla en formato corto
+DateTime fecha;
+bool validarFecha = false;
+do
+{
+    Console.WriteLine("Ingrese una fecha (formato: dd/MM/yyyy): ");
+    validarFecha = DateTime.TryParse(Console.ReadLine(), out fecha);
+    if (validarFecha)
+    {
+        Console.WriteLine($"Fecha ingresada en formato corto: {fecha:d}");
+    }
+    else
+    {
+        Console.WriteLine("Valor ingresado no es una fecha válida. Intente nuevamente.");
+        validarFecha = false;
+    }
+}
