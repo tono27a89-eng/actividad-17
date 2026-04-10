eaf18e86832b7fc39c49e1b6e7796e268b14dd67
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-04-10 11:55:08 -0600

    03 pruebas

--- 03/Program.cs
+++ 03/Program.cs
@@ -1,3 +1,19 @@
 ﻿
 //Desarrolle un programa que solicite la cantidad de estudiantes en un curso,
 //validando que sea un número entero positivo, evitando valores negativos o letras
+int cantidadEstudiantes;
+bool validarCantidad = false;
+do
+{
+    Console.WriteLine("ingrese cantidad de estudiantes: ");validarCantidad = int.TryParse(Console.ReadLine(), out cantidadEstudiantes);
+    if (validarCantidad && cantidadEstudiantes > 0)
+    {
+        Console.WriteLine("Cantidad de estudiantes ingresada: " + cantidadEstudiantes);
+    }
+    else
+    {
+        Console.WriteLine("Valor ingresado no es un número entero válido. Intente nuevamente.");
+        validarCantidad = false;
+    }
+}
+while (validarCantidad!=true);
